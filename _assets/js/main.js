3130aa78692c99061feac0f183d691c7e6fc5a38
Birgit <birgit.voslamber@secretescapes.com>
2020-09-09 10:52:52 +0200

    template copied, dynamic offers and photos added

---
+++ _assets/js/main.js
@@ -0,0 +1,76 @@
+// general js for the project that wouldn't be a reuseable component
+
+$('.js-image-attribution').each(function() {
+  var $this = $(this);
+  var contents = $this.html();
+  $this.css('cursor', 'pointer');
+  $this.html("<img style=\"width: 15px; height: 15px;\" src=\"_assets/img/icons/info.svg\" alt=\"picture information\">");
+  $this.on('click',function(){
+    $this.html(contents);
+    $this.css('cursor', 'auto');
+  });
+});
+
+
+
+
+/*********** Blur and change text colour of hero image  *************/
+
+
+$(window).on('scroll', function () {
+
+  if($(window).scrollTop() > 200) {
+         $(".bv-title-yellow").addClass("text--yellow");
+     } else {
+         //remove the background property so it comes transparent again (defined in your css)
+        $(".bv-title-yellow").removeClass("text--yellow");
+     }
+
+var isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1;
+
+    var pixs = $(document).scrollTop(),
+    pixs = pixs / 60,
+    offset = 600,
+    //range = 100,
+    calc = 0.8 - (pixs )/10;
+    //console.log(calc+' '+pixs);
+
+	if(isFirefox === false){
+    $(".bv-banner-out").css({"-webkit-filter": "blur("+pixs+"px)","filter": "blur("+pixs+"px)","opacity": calc });
+  } else { $(".bv-banner-out").css({"opacity": calc }); }
+});
+
+
+/*********** Play video  *************/
+
+// Get the modal
+var modal = document.getElementById('bv-myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("bv-myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("bv-close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+    $('body').addClass('bv-disable-scroll');
+		$('#video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'playVideo' + '","args":""}', '*');
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+    $('body').removeClass('bv-disable-scroll');
+		$('#video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+      $('body').removeClass('bv-disable-scroll');
+			$('#video')[0].contentWindow.postMessage('{"event":"command","func":"' + 'stopVideo' + '","args":""}', '*');
+  }
+}
---
+++ _pages/index.md
@@ -0,0 +1,21 @@
+---
+layout: single-page
+id: overview
+nav: true
+nav-order: 1
+title: Thailand
+
+banner:
+  title: Thailand
+  title-sup: Das Land des Lächelns
+  attribution: "<a href=\"#photo-link\">Photo</a> by username / <a rel=\"nofollow\" href=\"https://creativecommons.org/licenses/by/2.0/\">CC BY</a>"
+
+primary-cta:
+  label: Primary Call To Action
+  link: "#promo"
+
+
+headline: Lorem ipsum dolor sit amet
+intro: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos rerum optio est consequuntur ut quibusdam soluta molestias perspiciatis, dolor corrupti culpa labore aspernatur voluptates, ipsum laborum eligendi placeat et fugiat sit, quidem, totam iure ea ad velit! Aliquid, placeat, ea. Amet accusamus voluptas odit harum necessitatibus consequatur delectus autem rerum architecto voluptatum."
+
+---
